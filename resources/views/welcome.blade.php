fae767fc4b3a436c0a53e3bb0e9ca9d4c308afd6
nyusha1998 <nyusha.kononenko@live.com>
2018-05-29 14:47:28 +0300

    second nevblog

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel222
                 </div>
 
                  <p>
